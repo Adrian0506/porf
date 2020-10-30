1dc358f852e02244c99b0ef395d51006ec895176
Adrian0506 <60869100+Adrian0506@users.noreply.github.com>
2020-10-29 20:06:07 -0700

    added_gif

---
+++ js/index.js
@@ -0,0 +1,12 @@
+
+
+
+setTimeout(function () {
+document.querySelector('.loader').classList.add('dissapear');
+},1500);
+
+
+setTimeout(function () {
+    document.querySelector('.loader').style.display = "none";
+},3500);
+
